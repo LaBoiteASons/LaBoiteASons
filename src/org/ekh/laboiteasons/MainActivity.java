b5e04e6a83ca31b827fd935c5e2479271d8cc43f
Emeline Esteves <emelineesteves@gmail.com>
2013-03-07 00:50:06 +0100

    gridview + theme holo dark

--- src/org/ekh/laboiteasons/MainActivity.java
+++ src/org/ekh/laboiteasons/MainActivity.java
@@ -1,22 +1,43 @@
 package org.ekh.laboiteasons;
 
-import android.os.Bundle;
+import org.ekh.adapter.ImageAdapter;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	GridView gridView;
+
+	static final String[] MOBILE_OS = new String[] { 
+		"Android", "iOS","Windows", "Blackberry" };
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		gridView = (GridView) findViewById(R.id.gridView1);
 
+		gridView.setAdapter(new ImageAdapter(this, MOBILE_OS));
+
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Toast.makeText(
+						getApplicationContext(),
+						((TextView) v.findViewById(R.id.grid_item_label))
+						.getText(), Toast.LENGTH_SHORT).show();
+
+			}
+		});
+
+	}
 }
